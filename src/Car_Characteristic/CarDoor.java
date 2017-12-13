39c568ded422f98465783ef7c0e667157885d7cd
MYN <ricky.month@gmail.com>
2017-12-13 15:45:25 +0200

    Changed the choice of tasks to "Switch" and edited Cardoor

--- src/Car_Characteristic/CarDoor.java
+++ src/Car_Characteristic/CarDoor.java
@@ -1,5 +1,67 @@
 package Car_Characteristic;
 
 public class CarDoor {
+    public String door;
+    public String window;
 
+    /*public CarDoor(){
+        this("closed", "closed" );
+    }
+
+
+    public CarDoor(String door, String window){
+        this.door = door;
+        this.window = window;
+    }*/
+
+
+    public CarDoor (){
+        this("cloased", "cloased");
+    }
+    public CarDoor (String door, String window){
+        this.door = door;
+        this.window = window;
+    }
+
+
+    public void opendoor(){
+        this.door ="open";
+        show();
+    }
+    public void closeddoor(){
+        this.door = "closed";
+        show();
+    }
+
+    public void positiondoor(){
+        if (door == "closed"){
+            this.door = "open";
+            show();
+        }else if (door == "open"){
+            this.door = "closed";
+            show();
+        }
+    }
+    public void openwindow(){
+        this.window ="open";
+        show();
+    }
+    public void closewindow(){
+        this.window = "closed";
+        show();
+    }
+
+    public void positionwindow(){
+        if (window == "closed"){
+            this.window = "open";
+            show();
+        }else if (window == "open"){
+            this.window = "closed";
+            show();
+        }
+    }
+
+    public void show(){
+        System.out.println("The door was " + door + " and window is " + window);
+    }
 }
--- src/Car_Characteristic/Main.java
+++ src/Car_Characteristic/Main.java
@@ -1,7 +1,59 @@
 package Car_Characteristic;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        System.out.println("Hi!");
+        System.out.println("what do you want to do? \n" +
+                "1 - Choose a door and window");
+        Scanner scanner = new Scanner(System.in);
+        int choose = scanner.nextInt();
+        switch (choose){
+            case 1:
+                CarDoor cardoor1 = new CarDoor();
+                int repeat = 1;
+                while (repeat == 1) {
+                    System.out.println("Do you want open door? - 1 \n" +
+                            "Do you want closed door? - 2 \n" +
+                            "Do you want to change the position of the door? - 3 \n" +
+                            "(to defeult is closed) \n" +
+                            "Do you want open window? - 4 \n" +
+                            "Do you want closed window? - 5 \n" +
+                            "Do you want to change the position of the wndow? - 6 \n" +
+                            "(to defeult is closed)");
+                    int choosedoor = scanner.nextInt();
+                    switch (choosedoor){
+                        case 1:
+                            cardoor1.opendoor();
+                            break;
+                        case 2:
+                            cardoor1.closeddoor();
+                            break;
+                        case 3:
+                            cardoor1.positiondoor();
+                            break;
+                        case 4:
+                            cardoor1.openwindow();
+                            break;
+                        case 5:
+                            cardoor1.closewindow();
+                            break;
+                        case 6:
+                            cardoor1.positionwindow();
+                            break;
+                        default:
+                            System.out.println("Invalid number");
+                    }
+                    System.out.println("Choose the door and window again \n" +
+                            "1 - Yes \n" +
+                            "2 - No");
+                    repeat = scanner.nextInt();
+            }
+                break;
+            case 2:
+
+        }
 
     }
 }
